2f74bc4379162d33a2957047f7914e49d56a27c3
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2016-11-27 19:05:15 +0100

    Tidies up spacing

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const Botkit = require('botkit');
 const request = require('request');
-// const fetch = require('node-fetch');
-// const streamBuffers = require('stream-buffers');
 
 const createDictionary = require('./lib/wotd');
 
 if (!process.env.SLACK_TOKEN) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
+  console.log('Error: Specify token in environment');
+  process.exit(1);
 }
 
 const controller = Botkit.slackbot({
-    debug: false,
+  debug: false,
 });
 
 const bot = controller.spawn({
@@ -21,117 +19,101 @@ const bot = controller.spawn({
 const dictionary = createDictionary();
 
 const downloadMedia = url => (
-    Promise.resolve(request(url))
-    // fetch(url)
-    //     .then(response => response.buffer())
-    //     .then(buff => {
-    //         var stream = new streamBuffers.ReadableStreamBuffer({});
-    //         stream.put(buff);
-    //         return stream;
-    //     })
+  Promise.resolve(request(url))
 );
 
+
 const uploadFileToChannel = (url, word, locale, message) => {
-    downloadMedia(url)
-        .then(
-            file => {    
-                bot.api.files.upload({
-                    file,
-                    filename: `${word}-${locale}.mp3`,
-                    title: `${word} - ${locale}`,
-                    channels: message.channel
-                },
-                (err, { ok, file }) => {
-                    if (!err && ok === true) {
-                    }
-                })
-            },
-            err => console.error('Error fetching media', err)
-        )
-        .then(
-            null,
-            err => console.error('Error uploading media', err)
-        )
+  downloadMedia(url)
+    .then(
+      file => {    
+        bot.api.files.upload({
+          file,
+          filename: `${word}-${locale}.mp3`,
+          title: `${word} - ${locale}`,
+          channels: message.channel
+        });
+      },
+      err => console.error('Error fetching media', err)
+    )
+    .then(
+      null,
+      err => console.error('Error uploading media', err)
+    );
 }
 
 controller.hears(['wotd'], 'direct_message,direct_mention,mention', function(bot, message) {
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  }, function(err, res) {
+    if (err) {
+      bot.botkit.log('Failed to add emoji reaction :(', err);
+    }
+  });
 
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
+  dictionary.wordOfTheDay()
+    .then(
+      ({ word, uk, us }) => {
+        const replies = [ `Today's word is *${word}*` ];
+
+        if (!uk && !us) {
+          replies.push(`It's unpronounceable!`);
         }
-    });
 
-    dictionary.wordOfTheDay()
-        .then(
-            ({word, uk, us}) => {
+        bot.reply(message, replies.join(' '));
 
-                const replies = [
-                    `Today's word is *${word}*`
-                ];
+        if (uk) {
+          uploadFileToChannel(uk, word, 'UK', message);
+        }
 
-                if (!uk && !us) {
-                   replies.push(`It's unpronounceable!`);
-                }
-
-                bot.reply(message, replies.join(' '));
-
-
-                if (uk) {
-                    uploadFileToChannel(uk, word, 'UK', message);
-                }
-
-                if (us) {
-                    uploadFileToChannel(us, word, 'US', message);
-                }
-            },
-            (err) => bot.reply(message, `I'm having trouble finding a word`)
-        );
+        if (us) {
+          uploadFileToChannel(us, word, 'US', message);
+        }
+      },
+      (err) => bot.reply(message, `I'm having trouble finding a word`)
+    );
 
 });
 
 controller.hears(['how do you say (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    const requestedWord = message.match[1];
+  const requestedWord = message.match[1];
 
-    if (!requestedWord) {
-        return;
+  if (!requestedWord) {
+    return;
+  }
+
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  }, function(err, res) {
+    if (err) {
+      bot.botkit.log('Failed to add emoji reaction :(', err);
     }
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
+  });
 
     dictionary.word(requestedWord)
-        .then(
-            ({ word, uk, us }) => {
-                bot.reply(message, {
-                    text: `How do you say ${word} in <${uk}|UK> or <${us}|US>?`,
-                });
+      .then(
+        ({ word, uk, us }) => {
+          bot.reply(message, {
+              text: `How do you say ${word} in <${uk}|UK> or <${us}|US>?`,
+          });
 
-               if (!uk && !us) {
-                    bot.reply(message, `It's unpronounceable!`);
-                }
+          if (!uk && !us) {
+              bot.reply(message, `It's unpronounceable!`);
+          }
 
-                if (uk) {
-                    uploadFileToChannel(uk, word, 'UK', message);
-                }
+          if (uk) {
+              uploadFileToChannel(uk, word, 'UK', message);
+          }
 
-                if (us) {
-                    uploadFileToChannel(us, word, 'US', message);
-                }
-                
-            },
-            (err) => bot.reply(message, `I'm having trouble finding a word`)
-        );
+          if (us) {
+              uploadFileToChannel(us, word, 'US', message);
+          }
+        },
+        (err) => bot.reply(message, `I'm having trouble finding a word`)
+      );
 });
